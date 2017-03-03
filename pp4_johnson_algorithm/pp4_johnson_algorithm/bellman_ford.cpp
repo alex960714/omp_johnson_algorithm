495294225339adbb3327fc00d2a94890c31c3429
alex960714 <alex_9607@mail.ru>
2017-03-03 18:10:29 +0300

    Added Bellman-Ford algorithm implementation

--- pp4_johnson_algorithm/pp4_johnson_algorithm/bellman_ford.cpp
+++ pp4_johnson_algorithm/pp4_johnson_algorithm/bellman_ford.cpp
@@ -1,4 +1,33 @@
-bool Bellman_Ford(int **matr, int size, int vert, int *dist)
+#include <stdlib.h>
+
+bool Bellman_Ford(int **matr, int vert_num, int vert, int *dist)
 {
-	return false;
+	int curr_edge;
+	dist[vert] = 0;
+	for (int i = 0; i < vert_num; i++)
+		if (i != vert)
+			dist[i] = INT_MAX;
+	for (int i = 0; i < vert_num - 1; i++)
+	{
+		for (int j = 0; j < vert_num; j++)
+		{
+			for (int k = 0; k < vert_num; k++)
+			{
+				curr_edge = dist[j] + matr[j][k];
+				if (dist[k]>curr_edge)
+					dist[k] = curr_edge;
+			}
+		}
+	}
+
+	for (int j = 0; j < vert_num; j++)
+	{
+		for (int k = 0; k < vert_num; k++)
+		{
+			curr_edge = dist[j] + matr[j][k];
+			if (dist[k] > curr_edge)
+				return false;
+		}
+	}
+	return true;
 }
