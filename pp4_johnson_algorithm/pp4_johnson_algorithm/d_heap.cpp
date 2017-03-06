d1fb94d86eff3f9fe97154155918a7a54b704450
alex960714 <alex_9607@mail.ru>
2017-03-06 21:24:25 +0300

    Added d-heap implementation

---
+++ pp4_johnson_algorithm/pp4_johnson_algorithm/d_heap.cpp
@@ -0,0 +1,6 @@
+#include "d_heap.h"
+
+void d_heap::Swap(int node1, int node2)
+{
+	
+}
--- pp4_johnson_algorithm/pp4_johnson_algorithm/d_heap.h
+++ pp4_johnson_algorithm/pp4_johnson_algorithm/d_heap.h
@@ -1,9 +1,37 @@
 #ifndef _D_HEAP_H_
 #define _D_HEAP_H_
 
+#include <map>
+using namespace std;
+
+struct d_node 
+{
+	d_node *prev;
+	d_node **child;
+};
 
 class d_heap
 {
+private:
+	int d;
+	map<int,int> weight;
 
+	void Swap(int node1, int node2);
+	void Diving(int node);
+	void Emersion(int node);
+public:
+	d_heap(int deg = 2) { d = deg; };
+	~d_heap() {};
+
+	int GetLeft(int node);
+	int GetRight(int node);
+	int GetParent(int node);
+	int GetMinChild(int node);
+
+	void Insert(int w);
+	void DecreaseWeight(int node, int delta);
+	void DeleteMin();
+	void Delete(int node);
+	void MakeHeap(int *w, int num);
 };
 #endif
